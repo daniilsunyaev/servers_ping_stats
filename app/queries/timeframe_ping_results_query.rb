46240ce1473140cdfa6f6df621a724840b6387df
Daniil Sunyaev <dasforrum@gmail.com>
2021-02-09 18:03:00 +0300

    feat(i3): add ping results query

---
+++ app/queries/timeframe_ping_results_query.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class TimeframePingResultsQuery
+  attr_reader :relation, :host, :port, :timeframe_starts_at, :timeframe_ends_at
+
+  def initialize(relation = PingResult.all, host:, port:, timeframe_starts_at:, timeframe_ends_at: Time.current)
+    @relation = relation
+    @host = host
+    @port = port
+    @timeframe_starts_at = timeframe_starts_at
+    @timeframe_ends_at = timeframe_ends_at
+  end
+
+  def query
+    relation
+      .where(host: host, port: port, created_at: timeframe_starts_at..timeframe_ends_at)
+  end
+end
